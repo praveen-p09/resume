62217b24bbb59d89ae90f49542efc2d14d0395a1
Praveen Patro <praveen.patro911@gmail.com>
2023-04-23 17:45:22 +0530

    Updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,4 @@
-let burger = document.querySelector('.burger')
-
+let burger = document.querySelector('.burger');
 navbar = document.querySelector('.navbar');
 navlist = document.querySelector('.navlist');
 rightnav = document.querySelector('.rightnav');
